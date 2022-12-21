d0f776cbe66338a776ae3c3e0b4e5fb5910f9dd8
Miguel Ramos Matos <miguel-r-matos@alticelabs.com>
2022-12-21 21:12:50 +0000

    Added config-server-repository and kafka streams

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/listener/TwitterKafkaStatusListner.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/listener/TwitterKafkaStatusListner.java
@@ -4,7 +4,7 @@ import com.microservices.demo.config.KafkaConfigData;
 import com.microservices.demo.kafka.avro.model.TwitterAvroModel;
 import com.microservices.demo.kafka.producer.config.service.KafkaProducer;
 import com.microservices.demo.twitter.to.kafka.service.transformer.TwitterStatusToAvroTransformer;
-import org.checkerframework.checker.units.qual.K;
+//import org.checkerframework.checker.units.qual.K;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
